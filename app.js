83998ec11d39529772af4a85e8972b513c74bf0c
VINCENZO MEDICA <v.medica@studenti.unisa.it>
2025-01-16 16:44:22 +0100

    Aggiunta parte della normalizzazione

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const pca = require('./src/pca');
+const pathDtaset = pca.pcaWork('./Dataset/DatasetSpotify2000.csv', 0.70);
+
+/*
+
 const express = require('express');
 const app = express();
 
@@ -10,9 +15,8 @@ app.listen(port, () => {
     console.log(`Server in ascolto sulla porta ${port}`);
     console.log('Client al link: http://localhost:3000/');
 });
+*/
 
-const pca = require('./src/pca');
-const pathDtaset = pca.pcaWork('./Dataset/DatasetSpotify2000.csv', 0.70);
 
 
 
--- src/pca.js
+++ src/pca.js
@@ -1,7 +1,14 @@
-
+// Import delle librerie necessarie
+var nodeplotlib = require('nodeplotlib');
+var pcaLib = require('pca-js');
 var dataForge = require('data-forge');
+require('data-forge-fs');
 
-function pcaWork(datasetIniziale, percentuale){
+// Percorsi dei file per l'esportazione dei dataset
+const fileStandardizzato = './datasource/datasetNormalizzato.csv';
+const filePCA = './datasource/datasetPCA.csv';
+
+function pcaWork(datasetIniziale, sogliaVarianza){
     // Caricamento del dataset dal file
     let dataset = dataForgeLib.readFileSync(fileOrigine).parseCSV();
 
@@ -16,11 +23,43 @@ function pcaWork(datasetIniziale, percentuale){
     var datiCompleti = dataset.toArray();
 
     let datiNormalizzati = [];
-
+    /*
+     Itera sulle colonne del dataset
+     Estrae i valori di ciascuna colonna e li normalizza con lo z-score
+     I risultati normalizzati vengono salvati in un array
+     */
     for (var colonna of colonneArray) {
         let colonnaValori = [...colonna.series.content.values];
         normalizzaZScore(colonnaValori);
         datiNormalizzati.push(colonnaValori);
     }
 
+    // Creazione di un nuovo dataset normalizzato
+    let datasetNormalizzato = [];
+    for (let i = 0; i < datiNormalizzati[0].length; i++) {
+        let canzone = {};
+        canzone[tutteLeColonne[0]] = datiCompleti[i].Index;
+        canzone[tutteLeColonne[1]] = datiCompleti[i].Title;
+        canzone[tutteLeColonne[2]] = datiCompleti[i].Artist;
+        canzone[tutteLeColonne[3]] = datiCompleti[i]['Top Genre'];
+        for (let j = 0; j < datiNormalizzati.length; j++) {
+            let nomeColonna = colonneSelezionate[j];
+            canzone[nomeColonna] = datiNormalizzati[j][i];
+        }
+        datasetNormalizzato.push(canzone);
+    }
+
+    // Esportazione del dataset normalizzato
+    let dataFrameNormalizzato = new dataForgeLib.DataFrame({
+        values: datasetNormalizzato
+    });
+    dataFrameNormalizzato.asCSV().writeFileSync(fileStandardizzato);
+
+    // Applicazione della PCA
+    let datasetArray = dataFrameNormalizzato.subset(colonneSelezionate).toRows();
+    let autovettori = pcaLib.getEigenVectors(datasetArray);
+
+
+
+    //return [standardizzatoEsportato, pcEsportato];
 }
