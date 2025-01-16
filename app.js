d0b65487a52025ee0685da2cfa5f11dc5bc71556
VINCENZO MEDICA <v.medica@studenti.unisa.it>
2025-01-16 19:45:20 +0100

    Correzione file app e pca

--- app.js
+++ app.js
@@ -2,40 +2,6 @@ const pca = require('./src/pca');
 const kmeans = require('./src/kmeans');
 const dbscan = require('./src/dbscan');
 
-// Processo PCA
-const percorsiDataset = pca.analisiPCA('./Dataset/DatasetSpotify2000.csv', 0.70); // Array con i percorsi: [file standardizzato, file PCA]
-const percorsoStandardizzato = percorsiDataset[0];
-const percorsoPCA = percorsiDataset[1];
-
-// Processo K-Means
-const risultatiKMeans = kmeans.mainKMeans(percorsoPCA, percorsoStandardizzato);
-const clustersKMeans = risultatiKMeans[0];
-const datasetClusterizzato = risultatiKMeans[2];
-const tutteLeCanzoni = risultatiKMeans[3];
-
-// Generazione grafici per K-Means
-kmeans.grafico3D(clustersKMeans, datasetClusterizzato, tutteLeCanzoni);
-kmeans.graficoRadar(clustersKMeans, datasetClusterizzato, tutteLeCanzoni);
-kmeans.makeHistograms(clustersKMeans, "Danceability", datasetClusterizzato, tutteLeCanzoni);
-
-// Processo DBScan
-dbscan.dbscan(percorsoPCA);
-/*
-
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Ciao, mondo!');
-});
-
-const port = 3000;
-app.listen(port, () => {
-    console.log(`Server in ascolto sulla porta ${port}`);
-    console.log('Client al link: http://localhost:3000/');
-});
-*/
-
-
-
-
+const pathDataset= pca.pcaProcess('./Dataset/DatasetSpotify2000.csv',0.70); // Array, primo elemento path standardizzato secondo elemento path pc
+const pathStandardizzato = pathDataset[0];
+const pathPC = pathDataset[1];
--- src/pca.js
+++ src/pca.js
@@ -1,169 +1,303 @@
-// Import delle librerie necessarie
 var nodeplotlib = require('nodeplotlib');
-var pcaLib = require('pca-js');
-var dataForge = require('data-forge');
+var pca = require('pca-js');
+var dataForgeLib = require('data-forge');
 require('data-forge-fs');
 
-// Percorsi dei file per l'esportazione dei dataset
-const fileStandardizzato = './datasource/datasetNormalizzato.csv';
-const filePCA = './datasource/datasetPCA.csv';
+const standardizzatoEsportato = './Dataset/datasetNormalizzato.csv';
+const pcaOutputPath = './Dataset/datasetPCAOutput.csv';
 
-function pcaWork(datasetIniziale, sogliaVarianza){
-    // Caricamento del dataset dal file
-    let dataset = dataForgeLib.readFileSync(fileOrigine).parseCSV();
+function pcaWork(datasetDiPartenza,percentualeOttima){
 
-    // Selezione delle colonne di interesse (caratteristiche musicali)
-    let colonneSelezionate = ["Beats Per Minute (BPM)", "Energy", "Danceability", "Loudness (dB)", "Liveness", "Valence", "Acousticness", "Speechiness"];
-    let tutteLeColonne = ["Index", "Title", "Artist", "Top Genre", ...colonneSelezionate];
-    let sottoDataset = dataset.subset(colonneSelezionate);
+    // Carico il DataSet
+    let dataFrame = dataForgeLib.readFileSync(datasetDiPartenza).parseCSV();
 
-    // Normalizzazione dei dati con z-score
-    var colonne = sottoDataset.getColumns();
-    var colonneArray = colonne.toArray();
-    var datiCompleti = dataset.toArray();
+    // Definisce le features (caratteristiche) su cui applicare la PCA
+    let features = ["Beats Per Minute (BPM)", "Energy","Danceability","Loudness (dB)","Liveness","Valence","Acousticness","Speechiness"];
+    let totalFeatures=["Index","Title","Artist","Top Genre","Beats Per Minute (BPM)", "Energy","Danceability","Loudness (dB)","Liveness","Valence","Length (Duration)","Acousticness","Speechiness"];
+    let subDataFrame = dataFrame.subset(features);
 
-    let datiNormalizzati = [];
-    /*
-     Itera sulle colonne del dataset
-     Estrae i valori di ciascuna colonna e li normalizza con lo z-score
-     I risultati normalizzati vengono salvati in un array
-     */
-    for (var colonna of colonneArray) {
-        let colonnaValori = [...colonna.series.content.values];
-        normalizzaZScore(colonnaValori);
-        datiNormalizzati.push(colonnaValori);
+    // Converte le colonne in array
+    var columns = subDataFrame.getColumns();
+    var arrayOfColumns = columns.toArray();
+    var arrayCompleto= dataFrame.toArray();
+
+    let myColumns = []; // Array per le colonne standardizzate
+
+    // Standardizza ogni colonna
+    for (var column in arrayOfColumns) {
+        let myColumn = [];
+        myColumn.push(...arrayOfColumns[column].series.content.values);
+        standardize(myColumn);
+        myColumns.push(myColumn);
     }
 
-    // Creazione di un nuovo dataset normalizzato
-    let datasetNormalizzato = [];
-    for (let i = 0; i < datiNormalizzati[0].length; i++) {
-        let canzone = {};
-        canzone[tutteLeColonne[0]] = datiCompleti[i].Index;
-        canzone[tutteLeColonne[1]] = datiCompleti[i].Title;
-        canzone[tutteLeColonne[2]] = datiCompleti[i].Artist;
-        canzone[tutteLeColonne[3]] = datiCompleti[i]['Top Genre'];
-        for (let j = 0; j < datiNormalizzati.length; j++) {
-            let nomeColonna = colonneSelezionate[j];
-            canzone[nomeColonna] = datiNormalizzati[j][i];
+
+    let newDataSource = [];
+    for(let i = 0; i < myColumns[0].length; i++) {
+
+        let mySong = { };
+        mySong[totalFeatures[0]]=arrayCompleto[i].Index;
+        mySong[totalFeatures[1]]=arrayCompleto[i].Title;
+        mySong[totalFeatures[2]]=arrayCompleto[i].Artist;
+        mySong[totalFeatures[3]]=arrayCompleto[i]['Top Genre']; //Senza le virgolette, il nome della chiave sarebbe interpretato come due variabili separate e il codice non funzionerebbe.
+        // Aggiungi i dati delle features
+        for(let j = 0; j < myColumns.length; j++){
+            let name = features[j];
+            mySong[name] = myColumns[j][i];
         }
-        datasetNormalizzato.push(canzone);
+        newDataSource.push(mySong);
+
     }
 
-    // Converte il DataFrame normalizzato in formato CSV e lo scrive su un file specificato da 'fileStandardizzato'.
-    let dataFrameNormalizzato = new dataForgeLib.DataFrame({
-        values: datasetNormalizzato
+    // Crea un nuovo DataFrame con i dati normalizzati
+    let normalizedDataFrame  = new dataForgeLib.DataFrame({
+        values: newDataSource
     });
-    dataFrameNormalizzato.asCSV().writeFileSync(fileStandardizzato);
 
-    // Applicazione della PCA
-    let datasetArray = dataFrameNormalizzato.subset(colonneSelezionate).toRows();
-    let autovettori = pcaLib.getEigenVectors(datasetArray);
+    // Esporta il dataset normalizzato in CSV
+    normalizedDataFrame.asCSV().writeFileSync(standardizzatoEsportato );
+    subDataFrame = normalizedDataFrame .subset(features);
 
-    // Calcolo della percentuale di varianza spiegata per determinare il numero ottimale di componenti
-    let vettoriSelezionati = [];
+    // Converte il Dataset in array di righe
+    let data = subDataFrame.toRows();
+
+    // Numero di elementi (canzoni) nel DataSet
+    let dimension = data.length;
+
+    // Calcola gli autovettori e gli autovalori per la PCA
+    var vectors = pca.getEigenVectors(data);
+
+    //Percentuale di accuratezza considerando il primo, i primi due e i primi tre autovettori
+    console.log("Percentuali di varianza calcolati: ");
+    let v = [];
     let percentualiVarianza = [];
+    let percentuale;
     let i;
-    for (i = 0; i < autovettori.length; i++) {
-        vettoriSelezionati.push(autovettori[i]);    // 1. Aggiunge il vettore corrente ('autovettori[i]') all'array dei vettori selezionati.
-        let percentuale = pcaLib.computePercentageExplained(autovettori, ...vettoriSelezionati); // 2. Calcola la percentuale di varianza spiegata usando una funzione della libreria PCA ('computePercentageExplained').
-        percentualiVarianza.push(percentuale); // 3. Aggiunge la percentuale calcolata all'array delle percentuali di varianza ('percentualiVarianza').
-        if (percentuale >= sogliaVarianza) { // 4. Se la percentuale di varianza spiegata raggiunge o supera una soglia prefissata ('sogliaVarianza'),
-            break;  //    il ciclo termina, avendo selezionato il numero ottimale di componenti principali.
+    for(i = 0; i < vectors.length; i++) {
+        v.push(vectors[i]);
+        percentuale=pca.computePercentageExplained(vectors, ...v);
+        percentualiVarianza.push(percentuale);
+        console.log(i+1 +" : "+percentuale);
+        if(percentuale>=percentualeOttima) {
+            break;
         }
     }
+    // Grafico della varianza delle componenti principali
+    graficoComponentiVarianza(vectors);
 
-    // Generazione del grafico delle componenti principali
-    creaGraficoVarianza(autovettori);
+    // Calcola i dati trasformati (adattati) in base alle componenti principali
+    let adjustedData  = pca.computeAdjustedData(data, ...v).adjustedData; // N.B. Array di array
 
-    // Trasformazione dei dati usando le componenti principali
-    let datiTrasformati = pcaLib.computeAdjustedData(datasetArray, ...vettoriSelezionati).adjustedData;
 
-    // Creazione di un nuovo dataset con le componenti principali
-    let datasetPCA = [];
-    let nomiComponenti = [];
-    for (let j = 0; j < datiTrasformati.length; j++) {
-        nomiComponenti.push('PC' + (j + 1));
+    // Nuovo DataSource che conterrà il DataSet trasformato in base alle Componenti Principali individuate.
+    let newDataSourcePCA  = [];
+    let elementi=[];
+    //For per ottenere le scritte PC1,PC2... in base a quanti vettori ho usato per la pca
+    for(j=0;j<adjustedData .length;j++) {
+        elementi.push('PC'+(j+1));
     }
 
-    for (let i = 0; i < datiTrasformati[0].length; i++) {
-        let canzoneTrasformata = {};
-        for (let j = 0; j < datiTrasformati.length; j++) {
-            canzoneTrasformata[nomiComponenti[j]] = datiTrasformati[j][i];
-        }
-        datasetPCA.push(canzoneTrasformata);
+    // Riempo il nuovo DataSource
+    for (let i = 0; i < dimension; i++){
+
+        let mySong = {};
+        for(j=0;j<adjustedData .length;j++)
+            mySong[elementi[j]] = adjustedData[j][i];
+
+
+        newDataSourcePCA .push(mySong);
+
     }
 
-    // Esportazione del dataset con le componenti principali
-    let dataFramePCA = new dataForgeLib.DataFrame({
-        values: datasetPCA
+    // Creo nuovo DataFrame dal DataSource e poi lo esporto
+    let newDataFrame2 = new dataForgeLib.DataFrame({
+        values: newDataSourcePCA
     });
-    dataFramePCA.asCSV().writeFileSync(filePCA);
+    newDataFrame2.asCSV().writeFileSync(pcaOutputPath);
 
-    return [fileStandardizzato, filePCA];
+    return [standardizzatoEsportato, pcaOutputPath];
 
-    //return [standardizzatoEsportato, pcEsportato];
 }
 
-// Funzione per creare un grafico delle componenti principali in base alla varianza
-function creaGraficoVarianza(autovettori) {
+function graficoComponentiVarianza(vectors){
+
+    let v = [];
     let percentualiVarianza = [];
-    let vettoriSelezionati = [];
-
-    for(let i=0;i<autovettori.length; i++){
-        vettoriSelezionati.push(autovettori[i]);
-
-        // Calcola la percentuale di varianza spiegata dalle componenti selezionate
-        let percentuale = pcaLib.computePercentageExplained(autovettori, ...vettoriSelezionati);
-        // Memorizza la percentuale calcolata nell'array 'percentualiVarianza'.
+    let percentuale;
+    let i;
+    for(i = 0; i < vectors.length; i++) {
+        v.push(vectors[i]);
+        percentuale=pca.computePercentageExplained(vectors, ...v);
         percentualiVarianza.push(percentuale);
     }
 
-    // Crea un array con i numeri progressivi delle componenti principali (es: 1, 2, 3...).
-    let componenti = [...Array(percentualiVarianza.length).keys()].map(x => x + 1);
+    let componenti = [...Array(++i).keys()].map(x=>x+1);
 
-    // Configura il tracciato del grafico per rappresentare la relazione tra numero di componenti
-    // e la percentuale di varianza spiegata.
-    var trace = {
-        x: componenti,                  // Asse X: numero cumulativo delle componenti principali.
-        y: percentualiVarianza,        // Asse Y: percentuale cumulativa di varianza spiegata.
-        type: 'scatter'                // Tipo di grafico: scatter (lineare con punti).
+    //Generazione del grafico
+    var trace1 = {
+        x: componenti,
+        y: percentualiVarianza,
+        type: 'scatter'
     };
-
-    // Configura il layout del grafico (titolo e assi).
+    var data = [trace1];
     var layout = {
-        title: 'Varianza spiegata dalle componenti principali', // Titolo del grafico.
-        xaxis: { title: 'Numero di Componenti' },               // Etichetta dell'asse X.
-        yaxis: { title: 'Percentuale di Varianza' }             // Etichetta dell'asse Y.
+        title: 'Variazione numero componenti in base alla varianza della PCA',
+        xaxis: {
+            title: 'Numero Componenti',
+        },
+        yaxis: {
+            title: 'Percentuale Varianza',
+        }
     };
 
-    // Disegna il grafico utilizzando la libreria 'nodeplotlib'.
-    nodeplotlib.plot([trace], layout);
+    nodeplotlib.plot(data,layout);
+
 }
 
-// Funzione per calcolare la media di un array di numeri.
-/*
-* Calcola la somma di tutti gli elementi nell'array.
-* Ogni elemento viene convertito in numero con parseFloat per gestire eventuali stringhe.
-*/
-function calcolaMedia(array) {
-    let somma = array.reduce((acc, val) => acc + parseFloat(val), 0);
-    return Math.sqrt(sommaQuadrati / array.length);
-}
-/* * Funzione per calcolare la deviazione standard di un array di numeri.
-*
-* La deviazione standard misura la dispersione dei valori rispetto alla media.
-*/
-function calcolaDeviazioneStandard(array, media) {
-    // Calcola la somma dei quadrati delle differenze tra ciascun valore dell'array e la media.
-    let sommaQuadrati = array.reduce((acc, val) => acc + Math.pow(parseFloat(val) - media, 2), 0);
-    // Ritorna la radice quadrata della somma dei quadrati divisa per il numero di elementi
-    return Math.sqrt(sommaQuadrati / array.length);
+function getStandardDeviation(numbersArr, meanVal) {
+
+    var SDprep = 0;
+    for(var key in numbersArr) {
+        if(numbersArr[key]!=""||numbersArr[key]){
+            SDprep += Math.pow((parseFloat(numbersArr[key]) - meanVal),2);
+        }
+    }
+    var SDresult = Math.sqrt(SDprep/numbersArr.length);
+    return SDresult;
+
 }
 
-function normalizzazioneZScore(){
-    const media = calcolaMedia(array);
-    const deviazione = calcolaDeviazioneStandard(array, media);
-    array.forEach((val, index) =>{
-        array[index] = (val - media)/deviazione;
-    })
+function getMean(array){
+    let n = array.length;
+    let sum = 0;
+    array.forEach((value, index, array)=>{
+        if(value!=""||value){
+            sum += parseInt(value);
+        } else {
+            n--;
+        }
+    });
+    return sum/n;
 }
+
+function standardize(array){
+    const mean = getMean(array);
+    const standardDeviation = getStandardDeviation(array, mean);
+    array.forEach((value, index, array)=>{
+        zscore = (value - mean)/standardDeviation;
+        array[index] = zscore;
+    });
+}
+
+function normalize(val, max, min) {
+    return (val - min) / (max - min);
+}
+
+/* Used in app.js as stand-alone */
+function graficoComonetiPrecisioneVarianza(datasetDiPartenza){
+    // Carico il DataSet
+    let dataFrame = dataForgeLib.readFileSync(datasetDiPartenza).parseCSV();
+
+    // Seleziono solo le features che mi interessano
+    let features = ["Beats Per Minute (BPM)", "Energy","Danceability","Loudness (dB)","Liveness","Valence","Length (Duration)","Acousticness","Speechiness"];
+    let totalFeatures=["Index","Title","Artist","Top Genre","Beats Per Minute (BPM)", "Energy","Danceability","Loudness (dB)","Liveness","Valence","Length (Duration)","Acousticness","Speechiness"];
+    let subDataFrame = dataFrame.subset(features);
+
+    var columns = subDataFrame.getColumns();
+    var arrayOfColumns = columns.toArray();
+    var arrayCompleto= dataFrame.toArray();
+
+    let myColumns = []; // Array di array (ogni array e' una colonna)
+
+    for (var column in arrayOfColumns) {
+        let myColumn = [];
+        myColumn.push(...arrayOfColumns[column].series.content.values);
+        standardize(myColumn);
+        myColumns.push(myColumn);
+    }
+
+    //Normalizzazione valori tra 0 e 1
+    for(i=0;i<myColumns.length;i++){
+        var max=-100,min=100;
+        //Ricero il massimo e il minimo
+        for(j=0;j<myColumns[i].length;j++){
+            if(max<myColumns[i][j])
+                max=myColumns[i][j];
+            if(min>myColumns[i][j])
+                min=myColumns[i][j];
+        }
+        //Normalizzo i valori
+        for(j=0;j<myColumns[i].length;j++){
+            myColumns[i][j]=normalize(myColumns[i][j],max,min);
+        }
+    }
+
+
+    let newDataSource = [];
+    for(let i = 0; i < myColumns[0].length; i++) {
+
+        let mySong = { };
+        mySong[totalFeatures[0]]=arrayCompleto[i].Index;
+        mySong[totalFeatures[1]]=arrayCompleto[i].Title;
+        mySong[totalFeatures[2]]=arrayCompleto[i].Artist;
+        mySong[totalFeatures[3]]=arrayCompleto[i]['Top Genre'];
+        for(let j = 0; j < myColumns.length; j++){
+            let name = features[j];
+            mySong[name] = myColumns[j][i];
+        }
+        newDataSource.push(mySong);
+
+    }
+
+    let normalizedDataFrame  = new dataForgeLib.DataFrame({
+        values: newDataSource
+    });
+
+
+    subDataFrame = normalizedDataFrame .subset(features);
+
+    // DataSet come array di array
+    let data = subDataFrame.toRows();
+
+
+
+    // Calcolo autovettori e autovalori per PCA
+    var vectors = pca.getEigenVectors(data);
+
+    //Percentuale di accuratezza considerando il primo, i primi due e i primi tre autovettori
+    console.log("Percentuali: ");
+    let v = [];
+    let percentuale;
+
+    let numeroComponentiUsate=[];
+    let percentuali=[];
+    for(let i = 0; i < vectors.length; i++) {
+        v.push(vectors[i]);
+        numeroComponentiUsate.push(v.length);
+        percentuali.push(pca.computePercentageExplained(vectors, ...v));
+    }
+
+
+    var trace1 = {
+        x: numeroComponentiUsate,
+        y: percentuali,
+        type: 'scatter'
+    };
+
+    var dataGrafico=[trace1];
+    var layout = {
+        title: 'Variazione numero componenti in base alla varianza della PCA',
+        xaxis: {
+            title: 'Numero Componenti',
+        },
+        yaxis: {
+            title: 'Percentuale Varianza',
+        }
+    };
+
+    nodeplotlib.plot(dataGrafico,layout);
+}
+
+exports.graficoComonetiPrecisioneVarianza=graficoComonetiPrecisioneVarianza;
+exports.pcaProcess = pcaWork;
+
