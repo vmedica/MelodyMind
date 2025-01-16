d34b6d0f6b48c52b5a0d3d3dc6dae9f9bf51eeb7
VINCENZO MEDICA <v.medica@studenti.unisa.it>
2025-01-16 18:16:57 +0100

    Aggiornamento file app e creazione file dbscan e kmeans

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const pca = require('./src/pca');
-const pathDtaset = pca.pcaWork('./Dataset/DatasetSpotify2000.csv', 0.70);
+const kmeans = require('./src/kmeans');
+const dbscan = require('./src/dbscan');
 
+// Processo PCA
+const percorsiDataset = pca.analisiPCA('./Dataset/DatasetSpotify2000.csv', 0.70); // Array con i percorsi: [file standardizzato, file PCA]
+const percorsoStandardizzato = percorsiDataset[0];
+const percorsoPCA = percorsiDataset[1];
+
+// Processo K-Means
+const risultatiKMeans = kmeans.mainKMeans(percorsoPCA, percorsoStandardizzato);
+const clustersKMeans = risultatiKMeans[0];
+const datasetClusterizzato = risultatiKMeans[2];
+const tutteLeCanzoni = risultatiKMeans[3];
+
+// Generazione grafici per K-Means
+kmeans.grafico3D(clustersKMeans, datasetClusterizzato, tutteLeCanzoni);
+kmeans.graficoRadar(clustersKMeans, datasetClusterizzato, tutteLeCanzoni);
+kmeans.makeHistograms(clustersKMeans, "Danceability", datasetClusterizzato, tutteLeCanzoni);
+
+// Processo DBScan
+dbscan.dbscan(percorsoPCA);
 /*
 
 const express = require('express');
